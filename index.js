4f0edd32e7824c101f606e07ac4ed122b1b89259
discordbotmakers <61979402+discordbotmakers@users.noreply.github.com>
2020-04-19 20:26:22 -0400

    This is the main file of the bot!

---
+++ index.js
@@ -0,0 +1,52 @@
+```js
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fs = require('fs');
+const path = require('path')
+const config = require ("./config.json"); 
+
+bot.on('ready', () => {
+    console.log('I am now working!')
+
+    bot.user.setActivity('Yawning', {type: "PLAYING"})
+})
+
+bot.on('message', async msg => {
+    if (msg.author.bot) return;
+    let cmdPrefix = "";
+    if(!msg.content.startsWith(cmdPrefix)) return;
+
+    const args = msg.content.slice(cmdPrefix.length);
+    console.log(args);
+
+    if (msg.content === cmdPrefix + 'test') {
+        
+        let embed = new Discord.MessageEmbed()
+        .setColor('#ff6efd')
+        .setDescription('Test **successful**!')
+
+        msg.reply(embed)
+    }
+
+    if (msg.content === cmdPrefix + 'help') {
+
+        let embed = new Discord.MessageEmbed()
+        .setColor("GREEN")
+        .setTitle('Commands')
+        .addField('-test', 'Tests to see if bot is online.')
+        .addField('-help', 'Provides this panel.')
+
+        msg.channel.send(embed)
+    }
+if(cmd === `${prefix}kick)}`
+if(!
+message.member.hasPermission("KICK_MEMBERS"))
+return message.reply('You cannot use this command. You need the Permission "KICK_MEMBERS" If you have the permission please retry.');
+  let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+if(!member)return
+message.channel.send
+        }
+    }
+});
+
+bot.login(token);
